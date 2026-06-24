c492ec8241dfbc4383196d54ccec2b17a746e526
OpTiC GhOsT <72472040+HamaUk@users.noreply.github.com>
2026-06-24 01:44:01 +0100

    feat: Add FFmpeg extension decoder for Media3

--- player/build.gradle.kts
+++ player/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.media3.datasource.okhttp)
     implementation(libs.media3.session)
     implementation(libs.media3.ui)
+    implementation(libs.media3.ffmpeg.decoder)
 
     // VLC
     implementation(libs.vlc.android)
